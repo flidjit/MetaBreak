055b86e9eaa3043dd5b242b4a4d48b002dcf9931
flidjit <flidjit@gmail.com>
2022-07-13 03:09:30 -0500

    Starting on main functionality...

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+from tkinter import *
 from tkinter import ttk
 import sys
 import pygame as pg
@@ -8,7 +8,23 @@ import math
 import pickle
 import base64
 from enum import Enum
+from gamewindows import ViewPort
 
 
+class MainWindow(Toplevel):
+    def __init__(self, master=None):
+        super().__init__(master=master, bg='black')
+        self.resizable(False, False)
+        self.view = ViewPort()
+        self.root = Tk()
+        self.root.withdraw()
+        self.tool_bar = None
+        # Buttons : Host Game, Join Game,
+        #           Campaign Editor, Sprite Editor, Sphere Editor,
+        #           Options, Quit
 
-
+    def update(self):
+        if self.tool_bar:
+            self.tool_bar.update()
+        self.view.scene.fill((10, 0, 10))
+        self.view.update()
